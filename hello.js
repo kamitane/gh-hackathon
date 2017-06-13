0be5b00f4b53cc28aed7a065790da34c5be35b30
unknown <>
2017-06-13 20:15:48 +0900

    Add basement

---
+++ hello.js
@@ -0,0 +1,14 @@
+﻿$(function() {
+  // DOM構築完了
+  
+  // コントローラの定義
+  var helloWorldController = {
+    __name: 'HelloWorldController', // <- カンマを忘れずに
+    '#button click': function() {
+      alert("こんにちは世界！");
+    }
+  };
+  
+  // コントローラ化する
+  h5.core.controller('body', helloWorldController);  
+});
